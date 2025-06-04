9930b8b47a1afb155dbbf066d11581e9a7115f66
Edwin kamau <166378939+K6EDWIN@users.noreply.github.com>
2025-06-04 15:58:19 +0300

    Registration backend setup

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
-// Create MySQL connection using env variables
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
 });
-
 // Connect to MySQL
 db.connect((err) => {
   if (err) {
@@ -20,9 +19,44 @@ db.connect((err) => {
   }
 });
 
-// Example route
 app.get('/', (req, res) => {
-  res.send('Hello from server with env vars!');
+    res.sendFile(path.join(__dirname, 'frontend', 'Registration.html'));
+});
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'Registration.html'));
+});
+app.post('/submit', async (req, res) => {
+    const {
+        username, email, password,role
+    } = req.body;
+
+    try {
+        // Hash the password before storing it
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // SQL query to insert form data into the personal_information table
+        const query = `
+            INSERT INTO Users (
+                name,email,password,role)
+            ) VALUES (?, ?, ?,?)
+        `;
+
+        // Insert data into the database
+        pool.query(query, [
+             username, email, hashedPassword,role
+        ], (err, results) => {
+            if (err) {
+                console.error('Error inserting data into the database:', err);
+                res.status(500).json({ message: 'An error occurred while saving your information.' });
+                return;
+            }
+            // Send a success response
+            res.status(200).json({ message: 'Signing up successful. Welcome to strathtank' });
+        });
+    } catch (error) {
+        console.error('Error processing request:', error);
+        res.status(500).json({ message: 'An error occurred while processing your request.' });
+    }
 });
 
 app.listen(PORT, () => {
